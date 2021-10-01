10ccd2971a83b1d84b2f42777a8758ed0355523c
Dargeo <alejoarve12@gmail.com>
2021-09-30 22:43:09 -0500

    Solucion de bugs

--- client.py
+++ client.py
@@ -19,10 +19,11 @@ def main():
     url = "http://100.25.117.202:5050"
     url_contents = urllib.request.urlopen(url).read()
     soup = bs4.BeautifulSoup(url_contents, "html.parser")
-    div = soup.find('li', id="ull")
+    div = soup.find_all('li', id="ull")
 
-    
-    print(div.strings)
+    for d in div:
+        print(d.string)
+
     nume= input('Digite el numero de lo que quiera hacer \n 1.) Añadir bucket \n 2.) Eliminar bucket\n')
     if nume == '1' :
         postTask(input("Ingrese el nombre del bucket \n"))
