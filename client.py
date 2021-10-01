1e0b1b651fe12b94708bad28ae734e4ea65a5fc5
Dargeo <alejoarve12@gmail.com>
2021-09-30 22:36:03 -0500

    Solucion de bugs

--- client.py
+++ client.py
@@ -9,12 +9,11 @@ def postTask(bucket):
     
     url = "http://100.25.117.202:5050"
     url_contents = urllib.request.urlopen(url).read()
-    
     soup = bs4.BeautifulSoup(url_contents, "html.parser")
     div = soup.find('ul', id='ull')
 
     
-    print(div.string)
+    print(div)
 
 def removtask(bucket):
     data = {'bucket': bucket}
